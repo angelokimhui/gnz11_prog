451164d1e09cbc6b5473495fa218e595c603dca4
Angelo Kim Hui V. Lim (Android Developer) <angelokimhui@gmail.com>
2022-04-20 20:16:33 +0800

    tel~ : import stdlib to fix warning fix implicit declaration warning when using system function

--- 1_telephone_system/C/main.c
+++ 1_telephone_system/C/main.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
 #define NmS 50
 #define NbS 15
@@ -8,18 +9,19 @@ int main(){
 
 	char cust_name[NmS],cust_num[NbS];
 	printf("Telephone System\n");
+	
 	printf("Customer Name : ");
 	fgets(cust_name, NmS, stdin);
 	if ((strlen(cust_name) > 0) && (cust_name[strlen (cust_name) - 1] == '\n')){
         	cust_name[strlen (cust_name) - 1] = '\0';
 	}
+	
 	printf("Customer Number : ");
 	scanf("%14s",cust_num);
 	if (strlen(cust_num)>13){
 		printf("You entered beyond the character limit, please try again.\n");
 		return 1;
 	}
-	printf("\nCustomer name is %s",cust_name);	
-
+	
 	return 0;
 }
