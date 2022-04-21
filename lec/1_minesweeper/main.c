7b3d79b42e946de9b9ad41d0d43901e57515f01f
Angelo Kim Hui V. Lim (Android Developer) <angelokimhui@gmail.com>
2022-04-21 21:51:13 +0800

    mine~ : initial code with array and srand for randomness of the bomb.

--- lec/1_minesweeper/main.c
+++ lec/1_minesweeper/main.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define x 9
+#define y 9
 
 int main(){
-	printf("Hello World");
+	srand(time(NULL));
+	int x_counter,y_counter;
+	int table[x][y];
+	for (x_counter=0;x_counter<x;x_counter++){
+		for(y_counter=0;y_counter<y;y_counter++){
+			table[x_counter][y_counter]=rand()%2;
+			printf("This is table [%d][%d] = %d \n",x_counter,y_counter,table[x_counter][y_counter]);
+		}
+	}
 }
