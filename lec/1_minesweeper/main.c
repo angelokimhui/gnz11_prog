a03dc24eaa49c726b9d8c47be358ad19ea494198
angelokimhui <82715920+angelokimhui@users.noreply.github.com>
2022-04-28 16:31:59 +0800

    mine~ : Finish the code and added the following
    -manual number of bombs to be scattered
    -display the coordinates of the bombas (lol)
    -added max_HP = 3
    -added max_moves = 10
    -expanded the size of the map XD
    
    Known Issues :
    -I'm so dumb so I interchanged x and y, I just interchanged the printf lol.

--- lec/1_minesweeper/main.c
+++ lec/1_minesweeper/main.c
@@ -1,107 +1,140 @@
+/*
+ *  Basic Minesweeper in C Language
+ *  Author :  Angelo Kim Hui Lim(GNz11)
+ *  Purpose : School Assignment
+ *  Last Modified : April 27, 2022 4:23PM
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
-#define x 8
-#define y 8
+#define x 80
+#define y 25
+#define num_bombs 5
+#define max_hp 3
+#define max_moves 10
 
 int main(){
 	srand(time(NULL));
-	int x_counter,y_counter;
-	int table[x][y],record[x][y],tmp=0;
+	int x_counter,y_counter,curr_hp=max_hp,avail_moves=max_moves;
+	int table[x][y],record[x][y],bombx[num_bombs],bomby[num_bombs],tmp=0;
+
+	//make sure arrays contains zeroes only (seems like some index' before have -3xxxxx value)
 	for (x_counter=0;x_counter<x;x_counter++){
 		for(y_counter=0;y_counter<y;y_counter++){
-			table[x_counter][y_counter]=rand()%2;
 			record[x_counter][y_counter]=0;
+			table[x_counter][y_counter]=0;
 		}
 	}
+	//end fill arrays with zeroes
+
+	//generate bombs (mark 1) and get their coordinates
+	for (int tmp_ctr=0;tmp_ctr<num_bombs;tmp_ctr++){
+        table[bomby[tmp_ctr]=rand()%80][bombx[tmp_ctr]=rand()%25]=1;
+	}
+    //end generate bombs
 	int userx,usery;
 	do {
-		system("clear");
-		char userchoice;
-		printf("\tMinesweeper\n\n");
-		printf("   0  1  2  3  4  5  6  7\n");
+		system("cls");
+		//diplay bombas
+		printf ("\n  Bomb Coordinates\n");
+        printf ("  Bomb\tx\ty");
+        for(int tmp_ctr=0;tmp_ctr<num_bombs;tmp_ctr++){
+            printf("\n  %d\t%d\t%d",tmp_ctr+1,bombx[tmp_ctr],bomby[tmp_ctr]);
+        }
+        //end display bombas
+		printf("\n\t\t\t\tMinesweeper\n\n");
+		printf("    0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24\n");
 		for (x_counter=0;x_counter<x;x_counter++){
 			for(y_counter=0;y_counter<y;y_counter++){
 				if(y_counter==0){
-					printf("%d ",tmp);
+                    if (tmp<10){
+                        printf("%d  ",tmp);
+                    }
+                    else{
+                        printf("%d ",tmp);
+                    }
 					tmp++;
 					if(record[x_counter][0]==0){
-						printf("[?]");	
+						printf("[?]");
 					}
 					else if (record[x_counter][0]==1){
 						printf("[ ]");
 					}
-					
+
 				}
 				else {
 					if(record[x_counter][y_counter]==0){
-						printf("[?]");	
+						printf("[?]");
 					}
 					else if (record[x_counter][y_counter]==1){
 						printf("[ ]");
 					}
-					if(y_counter==7){
+					if(y_counter==24){
 						printf("\n");
 					}
 				}
 			}
 		}
 		tmp=0;
-		
+        printf("\nCurrent HP = %d",curr_hp);
+        printf("\nAvailable Moves = %d",avail_moves);
 		printf("\n\nEnter the x and y coordinate for the block you want to reveal.\n");
-		printf("Type ''E'' in X coordinate to exit.\n");
-		printf("X coordinate : ");
-		scanf(" %c",&userchoice);
-		if (userchoice == 'e' || userchoice == 'E'){
-			system("clear");
-			printf("Exiting Minesweeper..\n");
-			exit(3);
-		}
-		else {
-			userx=(int)userchoice-48;
-		}
-		printf("Y coordinate : ");
+		printf("\nX coordinate : ");
 		scanf(" %d",&usery);
+		printf("Y coordinate : ");
+		scanf(" %d",&userx);
 		record[userx][usery]=1;
-	}while (record[userx][usery]!=table[userx][usery]);
-	
-	system("clear");
-	printf("\tMinesweeper\n\n");
-	printf("   0  1  2  3  4  5  6  7\n");
+		if (record[userx][usery]==table[userx][usery]){
+            curr_hp=curr_hp-1;
+		}
+		avail_moves=avail_moves-1;
+	}while ((curr_hp!=0) && (avail_moves!=0));
+
+	system("cls");
+	printf("\n\t\t\t\tMinesweeper\n\n");
+		printf("    0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24\n");
 		for (x_counter=0;x_counter<x;x_counter++){
 			for(y_counter=0;y_counter<y;y_counter++){
 				if(y_counter==0){
-					printf("%d ",tmp);
+                    if (tmp<10){
+                        printf("%d  ",tmp);
+                    }
+                    else{
+                        printf("%d ",tmp);
+                    }
 					tmp++;
-					if(record[x_counter][0]==0){
-						printf("[?]");	
-					}
-					else if (record[x_counter][0]==1&&table[x_counter][0]==0){
-						
-						printf("[ ]");
+					if(record[x_counter][0]==0&&table[x_counter][0]==0){
+						printf("[?]");
 					}
 					else if (record[x_counter][0]==1&&table[x_counter][0]==1){
-						
-						printf("[*]");
+                        printf("[0]");
 					}
-					
-				}
-				else {
-					if(record[x_counter][y_counter]==0){
-						printf("[?]");	
-					}
-					else if (record[x_counter][y_counter]==1&&table[x_counter][y_counter]==0){	
+					else if (record[x_counter][0]==1){
 						printf("[ ]");
 					}
-					else if (record[x_counter][y_counter]==1&&table[x_counter][y_counter]==1){	
-						printf("[*]");
+                    else if(table[x_counter][0]==1){
+                        printf("[0]");
+                    }
+				}
+				else {
+					if(record[x_counter][y_counter]==0&&table[x_counter][y_counter]==0){
+						printf("[?]");
 					}
-					
-					if(y_counter==7){
+					else if (record[x_counter][y_counter]==1&&table[x_counter][y_counter]==1){
+                        printf("[0]");
+					}
+					else if (record[x_counter][y_counter]==1){
+						printf("[ ]");
+					}
+                    else if(table[x_counter][y_counter]==1){
+                        printf("[0]");
+                    }
+                    if(y_counter==24){
 						printf("\n");
 					}
 				}
 			}
 		}
-	printf("\n\tGame Over!\n\n");
-	exit(3);
+	printf("\n\t\t\t\tVoilaaa\n\n");
+	exit(0);
 }
